8c805d5edcd919c2115d68442de33ccab5884fea
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-30 13:54:45 +0700

    Connect SignInModal with AuthContext

--- src/components/Modal/SignInModal.js
+++ src/components/Modal/SignInModal.js
@@ -5,6 +5,8 @@ import {Modal, Input, Tooltip, Icon} from 'antd';
 import styled from 'styled-components';
 import {withRouter} from 'react-router-dom';
 
+import AuthContext from '../../contexts/AuthContext';
+
 import handleSignIn from '../../utils/handleSignIn';
 
 function SignInModal(props: any) {
@@ -30,63 +32,75 @@ function SignInModal(props: any) {
     });
   }
 
-  function checkSignInStatus(data) {
-    setTimeout(() => {
-      setConfirmLoading(false);
+  function checkSignInStatus(data, setUserdata) {
+    // setTimeout(() => {
+    setConfirmLoading(false);
 
-      if (data.status === 200) {
-        setModalSignInVisible(false);
+    if (data.status === 200) {
+      setModalSignInVisible(false);
 
-        successLoginModal();
+      successLoginModal();
 
-        props.history.push('/');
-      } else {
-        errorLoginModal();
-      }
-    }, 1000);
+      setUserdata(data.data);
+    } else {
+      errorLoginModal();
+    }
+    // }, 1000);
   }
 
   return (
-    <Modal
-      title="Sign in to do something cooler"
-      centered
-      visible={modalSignInVisible}
-      confirmLoading={confirmLoading}
-      onCancel={() => setModalSignInVisible(false)}
-      onOk={() => {
-        setConfirmLoading(true);
-        handleSignIn(userKey, userPassword).then(data => {
-          checkSignInStatus(data);
-        });
-      }}>
-      <FormGroup>
-        <Input
-          value={userKey}
-          onChange={e => setUserKey(e.target.value)}
-          placeholder="Enter your username/email"
-          prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}} />}
-          suffix={
-            <Tooltip title="You can use either username or email to continue">
-              <Icon type="info-circle" style={{color: 'rgba(0,0,0,.45)'}} />
-            </Tooltip>
-          }
-        />
-      </FormGroup>
+    <AuthContext.Consumer>
+      {({setUserdata}) => {
+        return (
+          <Modal
+            title="Sign in to do something cooler"
+            centered
+            visible={modalSignInVisible}
+            confirmLoading={confirmLoading}
+            onCancel={() => setModalSignInVisible(false)}
+            onOk={() => {
+              setConfirmLoading(true);
+              handleSignIn(userKey, userPassword).then(data => {
+                checkSignInStatus(data, setUserdata);
+              });
+            }}>
+            <FormGroup>
+              <Input
+                value={userKey}
+                onChange={e => setUserKey(e.target.value)}
+                placeholder="Enter your username/email"
+                prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}} />}
+                suffix={
+                  <Tooltip title="You can use either username or email to continue">
+                    <Icon
+                      type="info-circle"
+                      style={{color: 'rgba(0,0,0,.45)'}}
+                    />
+                  </Tooltip>
+                }
+              />
+            </FormGroup>
 
-      <FormGroup>
-        <Input.Password
-          value={userPassword}
-          onChange={e => setUserPassword(e.target.value)}
-          placeholder="Enter your password"
-          prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}} />}
-          suffix={
-            <Tooltip title="Make sure you don't share your credential data">
-              <Icon type="info-circle" style={{color: 'rgba(0,0,0,.45)'}} />
-            </Tooltip>
-          }
-        />
-      </FormGroup>
-    </Modal>
+            <FormGroup>
+              <Input.Password
+                value={userPassword}
+                onChange={e => setUserPassword(e.target.value)}
+                placeholder="Enter your password"
+                prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}} />}
+                suffix={
+                  <Tooltip title="Make sure you don't share your credential data">
+                    <Icon
+                      type="info-circle"
+                      style={{color: 'rgba(0,0,0,.45)'}}
+                    />
+                  </Tooltip>
+                }
+              />
+            </FormGroup>
+          </Modal>
+        );
+      }}
+    </AuthContext.Consumer>
   );
 }
 
