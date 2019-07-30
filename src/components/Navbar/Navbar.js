92a823603d0a87f36540f2da6e5ea593fa2438cc
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-30 13:56:28 +0700

    Add user dropdown, connect with AuthContext

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,30 +1,65 @@
 // @flow
 
 import * as React from 'react';
-import {Button} from 'antd';
+import {Button, Avatar, Menu, Dropdown} from 'antd';
+import {Link, withRouter} from 'react-router-dom';
 
 import {NavbarContainer, NavbarPart} from './style';
 import SignInModal from '../Modal/SignInModal';
 
+import AuthContext from '../../contexts/AuthContext';
+
 function Navbar(props: any) {
   const {modalSignInVisible, setModalSignInVisible} = props;
 
-  return (
-    <NavbarContainer>
-      <NavbarPart>Logo</NavbarPart>
-      <NavbarPart justifyContent={'center'}>Searchbox</NavbarPart>
-      <NavbarPart justifyContent={'flex-end'}>
-        <Button type={'primary'} onClick={() => setModalSignInVisible(true)}>
-          Sign In
-        </Button>
-      </NavbarPart>
+  function handleLogout(setUserdata) {
+    localStorage.removeItem('user-data');
+    setUserdata({});
+  }
 
-      <SignInModal
-        modalSignInVisible={modalSignInVisible}
-        setModalSignInVisible={setModalSignInVisible}
-      />
-    </NavbarContainer>
+  const menuItems = setUserdata => (
+    <Menu>
+      <Menu.Item>
+        <Link to={'/profile'}>Profile</Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Button onClick={() => handleLogout(setUserdata)}>Logout</Button>
+      </Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <AuthContext.Consumer>
+      {({userdata, setUserdata}) => {
+        return (
+          <NavbarContainer>
+            <NavbarPart>Logo</NavbarPart>
+            <NavbarPart justifyContent={'center'}>Searchbox</NavbarPart>
+            <NavbarPart justifyContent={'flex-end'}>
+              {userdata ? (
+                <Dropdown
+                  overlay={() => menuItems(setUserdata)}
+                  placement="bottomRight">
+                  <Avatar icon="user" />
+                </Dropdown>
+              ) : (
+                <Button
+                  type={'primary'}
+                  onClick={() => setModalSignInVisible(true)}>
+                  Sign In
+                </Button>
+              )}
+            </NavbarPart>
+
+            <SignInModal
+              modalSignInVisible={modalSignInVisible}
+              setModalSignInVisible={setModalSignInVisible}
+            />
+          </NavbarContainer>
+        );
+      }}
+    </AuthContext.Consumer>
   );
 }
 
-export default Navbar;
+export default withRouter(Navbar);
