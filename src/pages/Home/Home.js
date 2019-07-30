09f3668225ab2cfe70beeb0506fb7b7cb3218b5e
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-30 14:51:48 +0700

    Add dummy data in Home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,14 +2,79 @@
 
 import * as React from 'react';
 
+import {List, Avatar, Icon} from 'antd';
+
 import {Layout, Content, Footer} from '../../components/Base';
 import Navbar from '../../components/Navbar/';
+
+const listData = [];
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'http://ant.design',
+    title: `ant design part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+      'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
+
+const IconText = ({type, text}: any) => (
+  <span>
+    <Icon type={type} style={{marginRight: 8}} />
+    {text}
+  </span>
+);
 
 function Home() {
   return (
     <Layout>
       <Navbar />
-      <Content>oke brpo</Content>
+      <Content>
+        <List
+          itemLayout="vertical"
+          size="large"
+          pagination={{
+            onChange: page => {
+              console.log(page);
+            },
+            pageSize: 3,
+          }}
+          dataSource={listData}
+          footer={
+            <div>
+              <b>ant design</b> footer part
+            </div>
+          }
+          renderItem={item => (
+            <List.Item
+              key={item.title}
+              actions={
+                [
+                  // <IconText type="star-o" text="156" />,
+                  // <IconText type="like-o" text="156" />,
+                  // <IconText type="message" text="2" />,
+                ]
+              }
+              extra={
+                <img
+                  width={272}
+                  alt="logo"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                />
+              }>
+              <List.Item.Meta
+                avatar={<Avatar src={item.avatar} />}
+                title={<a href={item.href}>{item.title}</a>}
+                description={item.description}
+              />
+              {item.content}
+            </List.Item>
+          )}
+        />
+        ,
+      </Content>
       <Footer>by siapa aja</Footer>
     </Layout>
   );
