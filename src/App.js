dc83f0f734cb4d0e37046ebb334068d9a2abf178
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-30 13:53:57 +0700

    Wrap with AuthContext

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 // @flow
 
 import * as React from 'react';
+
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import 'antd/dist/antd.css';
 
-import PrivateRoute from './components/PrivateRoute';
-
 import Home from './pages/Home';
 import DummyPage from './pages/DummyPage';
 
+import PrivateRoute from './components/PrivateRoute';
+
+import AuthContext from './contexts/AuthContext';
+
 function App() {
+  const [userData, setUserdata] = React.useState({});
+
+  React.useEffect(() => {
+    const savedUserData = localStorage.getItem('user-data');
+
+    setUserdata(savedUserData);
+  });
+
+  // function handleLogout() {
+  //   localStorage.removeItem('user-data');
+  // console.log('ok');
+  // }
+
   return (
-    <Router>
-      <Route exact path={'/'} component={() => <Home />} />
-      <PrivateRoute exact path={'/profile'} component={() => <DummyPage />} />
-    </Router>
+    <AuthContext.Provider
+      value={{
+        userdata: userData,
+        setUserdata,
+      }}>
+      <Router>
+        <Route exact path={'/'} component={() => <Home />} />
+        <PrivateRoute exact path={'/profile'} component={() => <DummyPage />} />
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
