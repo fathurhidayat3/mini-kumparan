a6f8cce7032ea22d15d9bd5396f2f2ef1e62bffc
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-30 14:51:17 +0700

    Change handleSignIn body with formData

--- src/utils/handleSignIn.js
+++ src/utils/handleSignIn.js
@@ -3,31 +3,29 @@
 type credentialType = {userKey: string, userPassword: string};
 type dataType = {token: string, payload: any};
 
-export default function(userKey: string, userPassword: string) {
-  const credential: credentialType = {
-    userKey,
-    userPassword,
+export default function(username: string, password: string, email: string) {
+  const credential: any = {
+    username,
+    password,
   };
 
-  return fetch('/', {
-    method: 'post',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(credential),
+  const url = 'http://e89f1cc8.ngrok.io/login';
+
+  const body = new FormData();
+  body.append('username', username);
+  body.append('password', password);
+  body.append('email', email);
+
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+    },
+    body,
   })
     .then(res => JSON.stringify(res))
     .then((data: any) => {
-      data = {
-        status: 200,
-        data: {
-          token: '123456',
-          payload: {
-            username: 'fathurhidayat3',
-            name: 'Fathur Hidayat',
-          },
-        },
-      };
-
-      localStorage.setItem('user-data', JSON.stringify(data.data));
+      localStorage.setItem('user-data', JSON.stringify(data));
 
       return data;
     });
