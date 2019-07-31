8d8a98f4014749a93fc3acf055dba00f6b4787b3
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-31 12:10:13 +0700

    Add StoryDetail page

---
+++ src/pages/StoryDetail/StoryDetail.js
@@ -0,0 +1,90 @@
+// @flow
+
+import React from 'react';
+import {withRouter} from 'react-router-dom';
+import {Query} from 'react-apollo';
+import gql from 'graphql-tag';
+import styled from 'styled-components';
+import dayjs from 'dayjs';
+import {Card, Divider, Avatar} from 'antd';
+
+import Base from '../../components/Base';
+import HeadingText from '../../components/HeadingText';
+import Text from 'antd/lib/typography/Text';
+
+const GET_PUBLISHED_ARTICLE_BY_SLUG = gql`
+  query QueryGetPublishedArticleBySlug($slug: String!) {
+    GetPublishedArticleBySlug(slug: $slug) {
+      id
+      title
+      slug
+      body
+      thumbnail
+      status
+      createdAt
+      updatedAt
+      categories
+      user {
+        fullName
+      }
+    }
+  }
+`;
+
+function StoryDetail(props: any) {
+  const slug = props.history.location.pathname.split('/')[2];
+
+  return (
+    <Base>
+      <Query query={GET_PUBLISHED_ARTICLE_BY_SLUG} variables={{slug}}>
+        {({loading, error, data}) => {
+          if (loading) return 'Loading...';
+          if (error) return `Error! ${error.message}`;
+
+          const dataDetail = data.GetPublishedArticleBySlug;
+
+          return (
+            <StoryPanelContainer>
+              <div>
+                <HeadingText type={'h1'}>{dataDetail.title}</HeadingText>
+                <div
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    marginTop: 8,
+                  }}>
+                  <Avatar size={32} icon={'user'} />
+
+                  <div
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      margin: '0 8px',
+                    }}>
+                    <HeadingText type={'h4'}>
+                      {dataDetail.user.fullName}
+                    </HeadingText>
+
+                    <Text>{`Published at : ${dayjs(dataDetail.createdAt).format(
+                      'DD/MM/YYYY HH:mm'
+                    )}`}</Text>
+                  </div>
+                </div>
+              </div>
+
+              <Divider />
+
+              <div>
+                <Text>{dataDetail.body}</Text>
+              </div>
+            </StoryPanelContainer>
+          );
+        }}
+      </Query>
+    </Base>
+  );
+}
+
+const StoryPanelContainer = styled(Card)``;
+
+export default withRouter(StoryDetail);
---
+++ src/pages/StoryDetail/index.js
@@ -0,0 +1,11 @@
+// @flow
+
+import * as React from 'react';
+
+import StoryDetail from './StoryDetail';
+
+function StoryDetailContainer() {
+  return <StoryDetail></StoryDetail>;
+}
+
+export default StoryDetailContainer;
