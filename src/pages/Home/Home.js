0bcc02b06ab3ef7fb7f1a3cd63e038c9659e5ee8
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-31 14:52:20 +0700

    Refactor Home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,46 +4,14 @@ import * as React from 'react';
 
 import {List} from 'antd';
 import {Link} from 'react-router-dom';
-import {Query} from 'react-apollo';
-import gql from 'graphql-tag';
 import {Helmet} from 'react-helmet';
+
+import GetPublishedArticles from '../../graphql/Articles/QueryGetPublishedArticles';
 
 import FilterContext from '../../contexts/FilterContext';
 
 import Base from '../../components/Base';
 import StoryCard from '../../components/StoryCard';
-
-const listData = [];
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'http://ant.design',
-    title: `Story ke ${i + 1} created in North Pole with my Friend`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    thumbnail:
-      'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1564395669/kyfxbaax1mihfankpebg.jpg',
-    description:
-      'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    createdAt: '30 July 2019',
-    author: 'Joni Hermanto',
-    like: 156,
-    comment: 23,
-  });
-}
-
-const GET_PUBLISHED_ARTICLES = gql`
-  query QueryGetPublishedArticlesByCategory($category: String!) {
-    GetPublishedArticlesByCategory(category: $category) {
-      id
-      title
-      slug
-      thumbnail
-      createdAt
-      user {
-        fullName
-      }
-    }
-  }
-`;
 
 function Home() {
   return (
@@ -51,8 +19,8 @@ function Home() {
       {({filterData}) => {
         return (
           <Base>
-            <Query
-              query={GET_PUBLISHED_ARTICLES}
+            <GetPublishedArticles
+              query={GetPublishedArticles.query}
               variables={{category: filterData.category}}>
               {({loading, error, data}) => {
                 if (loading) return 'Loading...';
@@ -81,7 +49,7 @@ function Home() {
                   </>
                 );
               }}
-            </Query>
+            </GetPublishedArticles>
           </Base>
         );
       }}
