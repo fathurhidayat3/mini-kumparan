ab69c2b96a28a2769c3dbe12f09fffaad558a9bc
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-31 13:44:33 +0700

    Connect CategoryList to filter by category

--- src/components/Base/index.js
+++ src/components/Base/index.js
@@ -7,42 +7,68 @@ import {Layout, Content, Footer} from './style';
 import Navbar from '../Navbar';
 import HeadingText from '../HeadingText';
 
+import {colors} from '../../constants';
+
+import FilterContext from '../../contexts/FilterContext';
+
 function Base(props: any) {
   const {children} = props;
 
   const data = ['News', 'Politik', 'Entertainment', 'Otomotif'];
 
   return (
-    <Layout>
-      <Navbar />
+    <FilterContext.Consumer>
+      {({filterData, setFilterData}) => {
+        return (
+          <Layout>
+            <Navbar />
 
-      <Content>
-        <Row type={'flex'} justify={'center'}>
-          <Col span={5}>
-            <List
-              size="small"
-              header={
-                <div>
-                  <HeadingText type={'h4'}>Categories</HeadingText>
-                </div>
-              }
-              bordered
-              dataSource={data}
-              renderItem={item => <List.Item>{item}</List.Item>}
-              style={{background: 'white'}}
-            />
-          </Col>
+            <Content>
+              <Row type={'flex'} justify={'center'}>
+                <Col span={5}>
+                  <List
+                    size="small"
+                    header={
+                      <div>
+                        <HeadingText type={'h4'}>Categories</HeadingText>
+                      </div>
+                    }
+                    bordered
+                    dataSource={data}
+                    renderItem={item => (
+                      <List.Item
+                        style={{
+                          color:
+                            filterData.category === item.toUpperCase() &&
+                            colors.primary,
+                          fontWeight:
+                            filterData.category === item.toUpperCase() &&
+                            'bold',
+                          cursor: 'pointer',
+                        }}
+                        onClick={() =>
+                          setFilterData({category: item.toUpperCase()})
+                        }>
+                        {item}
+                      </List.Item>
+                    )}
+                    style={{background: 'white'}}
+                  />
+                </Col>
 
-          <Col span={9} offset={1}>
-            {children}
-          </Col>
+                <Col span={9} offset={1}>
+                  {children}
+                </Col>
 
-          <Col span={5} offset={1}></Col>
-        </Row>
-      </Content>
+                <Col span={5} offset={1}></Col>
+              </Row>
+            </Content>
 
-      <Footer>by siapa aja</Footer>
-    </Layout>
+            <Footer>by siapa aja</Footer>
+          </Layout>
+        );
+      }}
+    </FilterContext.Consumer>
   );
 }
 
