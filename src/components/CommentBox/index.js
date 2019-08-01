ec3e690c496388b59d5f903647df98ca36d851fb
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-08-01 10:46:33 +0700

    Add CommentBox comp

---
+++ src/components/CommentBox/index.js
@@ -0,0 +1,23 @@
+// @flow
+
+import React from 'react';
+import {Avatar, Comment} from 'antd';
+
+function CommentBox(props: any) {
+  const {avatar, value, handleChange, handleSubmit} = props;
+  return (
+    <Comment
+      avatar={<Avatar src={avatar} alt="Guest" icon={'user'} />}
+      content={
+        <CommentBox
+          onChange={handleChange}
+          onSubmit={handleSubmit}
+          // submitting={submitting}
+          value={value}
+        />
+      }
+    />
+  );
+}
+
+export default CommentBox;
