d6030be88fb6b20844ab306d782abea8eff00207
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-08-01 14:19:23 +0700

    Add generateSlug util func

---
+++ src/utils/generateSlug.js
@@ -0,0 +1,22 @@
+// @flow
+
+export default function generateSlug(str) {
+  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = str.toLowerCase();
+
+  const from = 'åàáãäâèéëêìíïîòóöôùúüûñç·/_,:;';
+  const to = 'aaaaaaeeeeiiiioooouuuunc------';
+
+  for (let i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  str = str
+    .replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+    .replace(/\s+/g, '-') // collapse whitespace and replace by -
+    .replace(/-+/g, '-') // collapse dashes
+    .replace(/^-+/, '') // trim - from start of text
+    .replace(/-+$/, ''); // trim - from end of text
+
+  return str;
+}
