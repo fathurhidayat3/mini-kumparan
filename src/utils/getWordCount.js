02687284f3fcd2c6214048492f5c1665be3a5d88
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-08-02 14:30:25 +0700

    Add getWordCount util func

---
+++ src/utils/getWordCount.js
@@ -0,0 +1,9 @@
+// @flow
+
+export default function getWordCount(editorState: any) {
+  const plainText = editorState.getCurrentContent().getPlainText('');
+  const regex = /(?:\r\n|\r|\n)/g; // new line, carriage return, line feed
+  const cleanString = plainText.replace(regex, ' ').trim(); // replace above characters w/ space
+  const wordArray = cleanString.match(/\S+/g); // matches words according to whitespace
+  return wordArray ? wordArray.length : 0;
+}
