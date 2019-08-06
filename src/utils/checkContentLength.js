e7d1b22fca2e6dfdb8078fb057a28c813c4a9920
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-08-07 00:38:33 +0700

    Change words to characters

--- src/utils/checkContentLength.js
+++ src/utils/checkContentLength.js
@@ -5,9 +5,9 @@ export default function checkContentLength(editorState: Object) {
   const editorContentlength = editorPlainText.length;
 
   if (editorContentlength === 0) {
-    return `Hello, you can write me 250 words`;
+    return `Hello, you can write me 250 characters`;
   } else if (0 < editorContentlength && editorContentlength < 250) {
-    return `Write ${250 - editorContentlength} more words`;
+    return `Write ${250 - editorContentlength} more characters`;
   } else if (editorContentlength < 999) {
     return `Good job, now you can publish your article`;
   }
